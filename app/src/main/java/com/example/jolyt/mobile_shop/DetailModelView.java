4964865b56f06236151a6751ef5bbfcccba12e1b
grimgor45 <seb.frkl@hotmail.fr>
2018-12-13 13:29:13 +0100

    recreated repo with gitignore

---
+++ app/src/main/java/com/example/jolyt/mobile_shop/DetailModelView.java
@@ -0,0 +1,153 @@
+package com.example.jolyt.mobile_shop;
+
+import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.SharedLibraryInfo;
+import android.databinding.BaseObservable;
+import android.databinding.BindingAdapter;
+import android.databinding.Observable;
+import android.databinding.ObservableArrayList;
+import android.databinding.ObservableField;
+import android.databinding.ObservableFloat;
+import android.databinding.ObservableInt;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.jolyt.mobile_shop.Database.DBOperation;
+import com.example.jolyt.mobile_shop.Database.Tables.Product;
+import com.example.jolyt.mobile_shop.Database.Tables.Shelf;
+
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+
+public class DetailModelView {
+    public final ObservableField<String> text = new ObservableField<>();
+    public final ObservableField<String> name = new ObservableField<>();
+    public final ObservableField<String> comment = new ObservableField<>();
+    public final ObservableField<String> shelf = new ObservableField<>();
+    public DetailFields detail;
+    public final ObservableField<String> buttonLabel = new ObservableField<>();
+    public final ObservableFloat price = new ObservableFloat();
+    private int idProduct;
+    public final ObservableArrayList<String> shelfList = new ObservableArrayList<>();
+    public final ObservableInt initialPos = new ObservableInt();
+    Realm re;
+
+    List<Shelf> listShelf;
+    RealmResults<Shelf> allShelf;
+
+    public DetailModelView(int idProduct){
+        re = Realm.getDefaultInstance();
+
+        DBOperation dbOp = new DBOperation(re);
+        this.idProduct = idProduct;
+        detail = new DetailFields();
+        RealmObject product = re.where(Product.class).equalTo("id", idProduct).findFirst();
+        name.set(((Product) product).getName());
+        comment.set(((Product) product).getCommentary());
+        shelf.set(((Product) product).getShelf().getName());
+        price.set(((Product) product).getPrice());
+        Log.i("testtest", ""+((Product) product).getPrice());
+        allShelf = re.where(Shelf.class).findAll();
+        buttonLabel.set("blablabla");
+        Log.i("ShelfIdInRealm", ""+((Product) product).getShelf().getId());
+        initialPos.set(allShelf.indexOf(allShelf.where().equalTo("name", ((Product) product).getShelf().getName()).findFirst()));
+        Log.i("ShelfIdinipos", ""+initialPos.get());
+        List<Shelf> listShelf= re.copyFromRealm(allShelf);
+        for (Shelf shel :  listShelf){
+            shelfList.add(shel.getName());
+        }
+
+    }
+
+    public void saveChanges(View view){
+        DBOperation dbOp = new DBOperation(re);
+        int idTrue = re.where(Shelf.class).equalTo("name",allShelf.get(initialPos.get()).getName()).findFirst().getId();
+        dbOp.updateProduct(idProduct, detail.getName(), idTrue, detail.getComment(), detail.getPrice());
+        Toast toast = Toast.makeText(view.getContext(), "update product", Toast.LENGTH_LONG);
+        Log.i("ShelfId", ""+initialPos.get());
+        Log.i("ShelfIdTrue", ""+idTrue);
+        toast.show();
+    }
+
+    public void createShelf(View view){
+
+        if (detail.getShelf() != null) {
+            DBOperation dbOp = new DBOperation(re);
+            Shelf shelf = new Shelf();
+            shelf.setId();
+            shelf.setName(detail.getShelf());
+            dbOp.createItem(shelf);
+            Toast toast = Toast.makeText(view.getContext(), "created shelf", Toast.LENGTH_LONG);
+            toast.show();
+
+        }
+        else
+        {
+            Toast toast = Toast.makeText(view.getContext(), "could not add shelf", Toast.LENGTH_LONG);
+            toast.show();
+
+        }
+    }
+
+    public void afterTextChange(CharSequence s)
+    {
+        detail.set(s.toString());
+    }
+    public void afterNameChange(CharSequence s)
+    {
+        Log.i("truc", s.toString());
+        detail.setName(s.toString());
+        name.set(s.toString());
+    }
+    public void afterCommentChange(CharSequence s)
+    {
+        detail.setComment(s.toString());
+        comment.set(s.toString());
+    }
+    public void afterPriceChange(CharSequence s)
+    {
+        Log.i("charseq", s.toString());
+        if (s.length() !=0) {
+            detail.setPrice(Float.valueOf(s.toString()));
+            Log.i("floatPrice", "" + Float.valueOf(s.toString()));
+            price.set(detail.getPrice());
+            Log.i("priceget", "" + price.get());
+        }
+    }
+
+    public void afterShelfNameCreation(CharSequence s){
+        if (s.length()!=0){
+            detail.setShelf(s.toString());
+        }
+    }
+
+
+    public void startAcitivityList(View view){
+        Context context = view.getContext();
+        Intent intent = new Intent(context, MainActivity.class);
+        re.close();
+        context.startActivity(intent);
+    }
+
+    public void deleteProduct(View view){
+        DBOperation dbOp = new DBOperation(re);
+        dbOp.deleteProductId(idProduct);
+        Toast toast = Toast.makeText(view.getContext(), "deleted product", Toast.LENGTH_LONG);
+        toast.show();
+        re.close();
+        startAcitivityList(view);
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/jolyt/mobile_shop/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.jolyt.mobile_shop;
+
+import android.content.Intent;
+import android.content.pm.SharedLibraryInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.jolyt.mobile_shop.Database.DBOperation;
+import com.example.jolyt.mobile_shop.Database.Tables.Cart;
+import com.example.jolyt.mobile_shop.Database.Tables.Product;
+import com.example.jolyt.mobile_shop.Database.Tables.Shelf;
+import com.example.jolyt.mobile_shop.Database.Tables.Users;
+
+import io.realm.Realm;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Do not touch needed for database
+
+        //toadd object  use : new oject then new transaction and copy
+        //for PK autogenerate
+        //mutableRealmInteger
+        //https://stackoverflow.com/questions/40174920/how-to-set-primary-key-auto-increment-in-realm-android
+
+        Realm re = Realm.getDefaultInstance();
+        DBOperation dbOp= new DBOperation(re);
+        Shelf shelf = new Shelf();
+        shelf.setId();
+        shelf.setName("Rayon Carottes");
+        dbOp.createItem(shelf);
+        shelf = dbOp.readWithId(re.where(Shelf.class).max("id").intValue(), Shelf.class);
+        Product product = new Product();
+        product.setName("Bannania");
+        product.setShelf(shelf);
+        product.setId();
+        dbOp.createItem(product);
+
+
+        //
+
+        /*for (int i = 0; i < re.where(Cart.class).findAll().size()+1; i++){
+            dbOp.deleteCartId(i);
+        }*/
+        Log.i("MaxProd", ""+re.where(Product.class).findAll().size());
+        re.close();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final Intent intent= new Intent(this, DetailActivity.class);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Realm re = Realm.getDefaultInstance();
+                int idProd = re.where(Product.class).findFirst().getId();
+                re.close();
+                intent.putExtra("idProduct", idProd);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
