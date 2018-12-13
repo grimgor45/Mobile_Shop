8c45d713b8a7641fa29c9b933353420b1e13d43c
grimgor45 <seb.frkl@hotmail.fr>
2018-12-13 13:27:25 +0100

    Cleaned repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# mobile_shop
-# mobile_shop
--- app/src/main/java/com/example/jolyt/mobile_shop/MainModelView.java
+++
@@ -1,9 +0,0 @@
-package com.example.jolyt.mobile_shop;
-
-import android.databinding.ObservableField;
-
-public class MainModelView {
-    public MainModelView(){
-
-    }
-}
