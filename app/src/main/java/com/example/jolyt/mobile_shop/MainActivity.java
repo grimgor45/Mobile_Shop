02abfde776ae73834e7df38dea57519d7b985efb
migabur <37173118+migabur@users.noreply.github.com>
2018-12-13 13:39:52 +0100

    trying

--- app/src/main/java/com/example/jolyt/mobile_shop/MainActivity.java
+++ app/src/main/java/com/example/jolyt/mobile_shop/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jolyt.mobile_shop;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.SharedLibraryInfo;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,12 +13,20 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.example.jolyt.mobile_shop.Database.DBOperation;
 import com.example.jolyt.mobile_shop.Database.Tables.Cart;
 import com.example.jolyt.mobile_shop.Database.Tables.Product;
 import com.example.jolyt.mobile_shop.Database.Tables.Shelf;
 import com.example.jolyt.mobile_shop.Database.Tables.Users;
+import com.example.jolyt.mobile_shop.databinding.ActivityMainBinding;
+
+import java.lang.reflect.ReflectPermission;
 
 import io.realm.Realm;
 
@@ -25,9 +35,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ((ActivityMainBinding)DataBindingUtil.setContentView(this,R.layout.activity_main)).setMainViewModel(new MainModelView());
 
-        //Do not touch needed for database
+
+                //Do not touch needed for database
 
         //toadd object  use : new oject then new transaction and copy
         //for PK autogenerate
@@ -38,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         DBOperation dbOp= new DBOperation(re);
         Shelf shelf = new Shelf();
         shelf.setId();
-        shelf.setName("Rayon Carottes");
+        shelf.setName("Rayon bananes");
         dbOp.createItem(shelf);
         shelf = dbOp.readWithId(re.where(Shelf.class).max("id").intValue(), Shelf.class);
         Product product = new Product();
@@ -46,6 +57,20 @@ public class MainActivity extends AppCompatActivity {
         product.setShelf(shelf);
         product.setId();
         dbOp.createItem(product);
+
+        long sizeOfProductTable = re.where(Product.class).count();
+
+
+        LinearLayout parent = findViewById(R.id.parentLayout);
+        for(int i=0;i<sizeOfProductTable;i++) {
+            LinearLayout Llayout = new LinearLayout(this);
+            Llayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            parent.addView(Llayout);
+            Button btn = new Button(this);
+            btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            btn.setText("this is working");
+            Llayout.addView(btn);
+        }
 
 
         //
@@ -65,11 +90,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Realm re = Realm.getDefaultInstance();
-                int idProd = re.where(Product.class).findFirst().getId();
-                re.close();
-                intent.putExtra("idProduct", idProd);
-
+                intent.putExtra("idProduct", 1);
                 startActivity(intent);
             }
         });
